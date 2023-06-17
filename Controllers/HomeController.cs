5b7c0f1454c63babae6842e650025670272659a1
Ddavila09 <ddavilax23@gmail.com>
2023-06-17 10:31:32 -0500

    LogNReg

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using LoginNReg.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace LoginNReg.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext db;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        db = context;
+    }
+    // -------------------------------------------------------
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+
+        return View("Index");
+    }
+    // -------------------------------------------------------
+    [HttpPost("register")]
+    public IActionResult Register(User newUser)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Index();
+        }
+        PasswordHasher<User> hashbrowns = new PasswordHasher<User>();
+        newUser.Password = hashbrowns.HashPassword(newUser, newUser.Password);
+
+        db.Users.Add(newUser);
+        db.SaveChanges();
+
+        HttpContext.Session.SetInt32("UUID", newUser.UserId);
+        return RedirectToAction("Success");
+    }
+    // -------------------------------------------------------
+    [HttpPost("login")]
+    public IActionResult Login(LoginUser loginUser)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Index();
+        }
+
+        User? dbUser = db.Users.FirstOrDefault(user => user.Email == loginUser.LoginEmail);
+        if (dbUser == null)
+        {
+            ModelState.AddModelError("LoginEmail", "not found.");
+            return Index();
+        }
+
+        PasswordHasher<LoginUser> hashBrowns = new PasswordHasher<LoginUser>();
+        PasswordVerificationResult pwCompareResult = hashBrowns.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+        if (pwCompareResult == 0)
+        {
+            // normally we wont be specific w/errors, but for demo we are
+            // since malicious users can benefit from specificity
+            ModelState.AddModelError("LoginPassword", "invalid password");
+
+        }
+
+        HttpContext.Session.SetInt32("UUID", dbUser.UserId);
+        return RedirectToAction("Success");
+    }
+    // -------------------------------------------------------
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+    // -------------------------------------------------------
+    [SessionCheck]
+    [HttpGet("/success")]
+    public IActionResult Success()
+    {
+        return View("Success");
+    }
+    // -------------------------------------------------------
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+// ------------------------------------------------------------
+
+// Name this anything you want with the word "Attribute" at the end
+public class SessionCheckAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Find the session, but remember it may be null so we need int?
+        int? userId = context.HttpContext.Session.GetInt32("UUID");
+        // Check to see if we got back null
+        if (userId == null)
+        {
+            // Redirect to the Index page if there was nothing in session
+            // "Home" here is referring to "HomeController", you can use any controller that is appropriate here
+            context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
